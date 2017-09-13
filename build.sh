839d40decba918fa8f2445fad6d10d66b31c8a11
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-09-13 14:41:36 +0200

    Updated image for latest xhgui version

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
-docker build -t "inet_php_xhgui" .
+
+set -e
+
+docker build -t "inet_phing_test" .
+
 echo ""
 echo ""
+
 if [ $? -eq 0 ]; then
-    # docker run -d --hostname xhgui --name xhgui inet_php_xhgui
-    echo -e "\x1b[1;32mBuild Done\e[0m"
+    echo -e "\x1b[1;32mBuild Done. To run it: \e[0m"
+    echo '  docker run -d --rm --hostname "phing-test-ctn" --name "phing-test-ctn" inet_phing_test'
+    echo '  docker exec -i -t "phing-test-ctn" /bin/bash'
+    echo "Once Done : "
+    echo '  docker stop "phing-test-ctn"'
+    echo ""
+    echo "Or if you want to directly enter the container, then remove it : "
+    echo '  docker run -ti --rm inet_phing_test /bin/bash'
 fi
