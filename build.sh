ed97eb518a7d7ea81eabae7edbbc9d3de1af0a29
Emmanuel Dyan <emmanueldyan@gmail.com>
2018-12-15 15:20:24 +0100

    Fixed all tests with regexp

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ -z "php$1" -o ! -d "php$1" ]; then
+if [ -z "$1" -o ! -d "$1" ]; then
     echo "You must define a valid PHP version to build as parameter (5.6 or 7.2)"
     exit 1
 fi
@@ -11,19 +11,19 @@ VERSION=$1
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-cd php$1
-docker build -t "edyan_xhgui_php${VERSION}_test" .
+cd $1
+docker build -t "edyan_xhgui_${VERSION}_test" .
 echo ""
 echo ""
 if [ $? -eq 0 ]; then
     echo -e "${GREEN}Build Done${NC}."
     echo ""
     echo "Run :"
-    echo "  docker run --rm --hostname xhgui_php${VERSION}-test-ctn --name xhgui_php${VERSION}-test-ctn edyan_xhgui_php${VERSION}_test"
-    echo "  docker exec -i -t xhgui_php${VERSION}-test-ctn /bin/bash"
+    echo "  docker run --rm --hostname xhgui_${VERSION}-test-ctn --name xhgui_${VERSION}-test-ctn edyan_xhgui_${VERSION}_test"
+    echo "  docker exec -i -t xhgui_${VERSION}-test-ctn /bin/bash"
     echo "Once Done : "
-    echo "  docker stop xhgui_php${VERSION}-test-ctn"
+    echo "  docker stop xhgui_${VERSION}-test-ctn"
     echo ""
     echo "Or if you want to directly enter the container, then remove it : "
-    echo "  docker run -ti --rm edyan_xhgui_php${VERSION}_test /bin/bash"
+    echo "  docker run -ti --rm edyan_xhgui_${VERSION}_test /bin/bash"
 fi
