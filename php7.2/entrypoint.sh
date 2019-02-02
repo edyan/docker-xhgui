2dc8464ae0d30c180613e8bb9b60c735a7eb43cc
Toshiyuki Goto <ngyuki.jp@gmail.com>
2019-02-02 20:29:00 +0900

    fixes #5 fpm and nginx config files will be empty at startup (#6)

--- php7.2/entrypoint.sh
+++ php7.2/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-envsubst '${NGINX_PORT},${MONGO_PORT},${PHPFPM_PORT}' < /etc/nginx/sites-available/default > /etc/nginx/sites-available/default
-envsubst '${PHPFPM_PORT}' < /etc/php/7.2/fpm/pool.d/www.conf > /etc/php/7.2/fpm/pool.d/www.conf 
+envsubst '${NGINX_PORT},${MONGO_PORT},${PHPFPM_PORT}' < /etc/nginx/sites-available/default.template > /etc/nginx/sites-available/default
+envsubst '${PHPFPM_PORT}' < /etc/php/7.2/fpm/pool.d/www.conf.template > /etc/php/7.2/fpm/pool.d/www.conf
 
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
